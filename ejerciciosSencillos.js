5d5ca8de5b41a5babdcd8276a16db8afa1511c02
Nancy20m4c <101662250+Nancy20m4c@users.noreply.github.com>
2024-11-01 13:08:39 +0100

    ejercicio CalculateSquare

---
+++ ejerciciosSencillos.js
@@ -0,0 +1,12 @@
+// Ejercicios 1.1
+function calculateSquare(number){
+    const square = number * number;
+
+    return square;
+}
+
+const result = calculateSquare(5);
+console.log(result);
+
+// Ejercicio 1.2
+
